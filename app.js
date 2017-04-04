9f0d2eaeb17edda001e37a4fd45961844107adc2
Mathew C Martin <chactas.stryker@gmail.com>
2017-04-04 13:46:43 -0700

    Problem 4 commit. 1 - 4 passing.

--- app.js
+++ app.js
@@ -81,12 +81,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var sumAandB = sum(testArray[0], testArray[1]);
+  var sumAandC = sum(sumAandB[0], testArray[2]);
+  var answerArray = sumAandC[0];
+  var messageArray = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + answerArray + ' is their sum.';
+  var outputArray = [answerArray, messageArray];
+  console.log(outputArray);
+  return outputArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
